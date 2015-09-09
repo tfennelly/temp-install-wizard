7f2877bb5d2d35e801faa324a7f8caaf6cf2f248
tfennelly <tom.fennelly@gmail.com>
2015-09-09 09:43:59 +0100

    Fix jQuery UI Dialog widget CSS namespacing

--- js/wizard.js
+++ js/wizard.js
@@ -3,24 +3,26 @@ var $ = jQueryUI.getJQueryUI();
 
 // Yeah, we'd replace this with a handlebars template ...
 var modal = 
-    $('<div class="jquery-ui-1">' +
-    '<div id="jenkins-plugin-wizard" title="Jenkins Install Wizard">' +
+    $('<div id="jenkins-plugin-wizard" title="Jenkins Install Wizard">' +
     '<p>' +
     '<span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>' +
     'Click OK to install....' +
     '</p>' +
-    '</div>' +
     '</div>');
 
 $('body').append(modal);
 
-$('#jenkins-plugin-wizard').dialog({
+modal.dialog({
+    dialogClass: "jenkins-plugin-wizard-dialog",
     modal: true,
     buttons: {
         Ok: function () {
             $(this).dialog("close");
-            $('#jenkins-plugin-wizard').remove();
+            modal.remove();
         }
     }
 });
+
+// Need to wrap. See https://github.com/jenkinsci/js-libs/tree/master/jquery-detached#namespacing-pitfalls
+$('.jenkins-plugin-wizard-dialog').wrap('<div class="jquery-ui-1"></div>');
 
